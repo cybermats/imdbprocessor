09e87fff669002d888ab010a2d6b631aaf12f109
Mats Fredriksson <cybermats@gmail.com>
2019-11-11 09:04:25 +0100

    Added the full infra structure

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -10,8 +10,68 @@ provider "google" {
   region = "us-central1"
 }
 
+variable "backend-name" {
+  default = "graph-backend"
+}
+
+variable "input-name" {
+  default = "graph-input"
+}
+
+variable "config_files" {
+  default = 'config/imdb-urls.tsv'
+}
+
 resource "google_storage_bucket" "backend-bucket" {
-  name = "graph-backend"
+  name = var.backend-name
   storage_class = "REGIONAL"
   location = "us-central1"
+}
+
+resource "google_storage_bucket_object" "imdbconfig" {
+  name = var.config_files
+  bucket = google_storage_bucket.backend-bucket.name
+  source = var.config_files
+}
+
+resource "google_storage_object_acl" "imdbconfig-acl" {
+  bucket = google_storage_bucket.backend-bucket.name
+  object = google_storage_bucket_object.imdbconfig.output_name
+
+  role_entity = [
+    "READER:user-allUsers"]
+}
+
+resource "google_storage_bucket" "input-bucket" {
+  name = var.input-name
+  storage_class = "REGIONAL"
+  location = "us-central1"
+}
+
+resource "google_storage_transfer_job" "imdb-nightly" {
+  description = "Nightly download of IMDB data"
+  schedule {
+    start_time_of_day {
+      hours = 3
+      minutes = 0
+      nanos = 0
+      seconds = 0
+    }
+    schedule_start_date {
+      day = 11
+      month = 11
+      year = 2019
+    }
+  }
+  transfer_spec {
+    http_data_source {
+      list_url = "https://storage.googleapis.com/graph-backend/config/imdb-urls.tsv"
+    }
+    transfer_options {
+      overwrite_objects_already_existing_in_sink = true
+    }
+    gcs_data_sink {
+      bucket_name = google_storage_bucket.backend-bucket.url
+    }
+  }
 }
